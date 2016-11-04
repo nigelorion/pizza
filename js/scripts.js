bc209a27b1840c15cd6092389f619837a5c9c6e0
Nigel Orion <Guest@epicodus-33.local>
2016-11-04 15:26:38 -0700

    Add complete logic for creating objects, output order information to user

--- js/scripts.js
+++ js/scripts.js
@@ -7,35 +7,34 @@ function User(name, street, city, state, zip, phone) {
   this.phone = phone;
 }
 
-function Pizza(size, toppings, price) {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  this.price = price;
 }
 
-function toppingsChecker(toppings) {
-
+Pizza.prototype.priceChecker = function() {
+  var price = this.toppings.length;
+  if (this.size === "Large") {
+    price = 21.99 + price;
+  } else  {
+    price = 12.99 + price;
+  }
+  return price;
 }
 
-function orderPrice(size, toppings) {
-   var price;
-   if(pizzaSize === medium) {
-     price = "$12.99";
-   } else {
-     price = "$21.99";
-   }
-   return price;
- };
+User.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
+}
 
 
 $(document).ready(function() {
-
 
   $("#order-type-next").on('click', function() {
   $("#order-type").fadeOut(200);
   $("#account-form").delay(100).fadeIn(600);
 
   });
+
   $("#account-next").on('click', function() {
   $("#account-form").fadeOut(200);
   $("#pizza-form").delay(100).fadeIn(600);
@@ -45,30 +44,29 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-
     $("#account").each(function() {
       var name = $(this).find("#name").val();
       var street = $(this).find("#street").val();
       var city = $(this).find("#city").val();
       var state = $(this).find("#state").val();
+      var zip = $(this).find("#zip").val();
       var phoneNumber = $(this).find("#number").val();
 
-      var newUser = new User(name, street, city, state, phoneNumber);
-      console.log(newUser);
+    var newUser = new User(name, street, city, state, zip, phoneNumber);
+
+    var size = $("#size").val();
+    var toppings = $('input:checkbox:checked').map(function() {
+    return this.value;
+    }).get();
+    var newPizza = new Pizza(size, toppings);
+    (newUser.fullAddress());
+
+    $("#userAddress").text(newUser.fullAddress());
+    $("#userName").text(newUser.name);
+    $("#userPrice").text(newPizza.priceChecker());
+    $("#contactNumber").text(newUser.phone)
+    $("#pizza-form").fadeOut(200);
+    $("#order-confirmation").delay(200).fadeIn(200);
     });
-
-    var newPizza = new Pizza(name, street);
-    console.log(newPizza);
-
   });
-  // $("#order-type-next").on('click', function() {
-  // $("#order-type").fadeOut(200);
-  // $("#account-form").delay(100).fadeIn(600);
-  //
-  // });
-  //
-  // var newAccount = new User()
-
-
-
 });
